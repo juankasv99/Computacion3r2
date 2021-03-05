0a3c07d6d3799e65615801f07199fa7361f72cb4
Juan Carlos Soriano <thelakegray@gmail.com>
2021-03-05 19:37:36 +0100

    repte 2 casi acabado

--- 2n Semestre/VC/Reptes/Repte2/repte2.m
+++ 2n Semestre/VC/Reptes/Repte2/repte2.m
@@ -1,31 +1,60 @@
 video_llum = VideoReader("./DSC_0524.MOV");
 video_fosc = VideoReader("./DSC_0525.MOV");
 
-frame = rgb2gray(readFrame(video_llum));
-sz = size(frame);
+
+
+frame_llum = rgb2gray(readFrame(video_llum));
+frame_fosc = rgb2gray(readFrame(video_fosc));
+sz_llum = size(frame_llum);
+sz_fosc = size(frame_fosc);
 N = 200;
-acum = zeros(sz(1)/2,sz(2)/2,N);
-acum(:,:,1) = frame(1:2:end, 1:2:end, 1);
+acum_llum = zeros(sz_llum(1)/2,sz_llum(2)/2,N);
+acum_llum(:,:,1) = frame_llum(1:2:end, 1:2:end, 1);
+acum_fosc = zeros(sz_fosc(1)/2,sz_fosc(2)/2,N);
+acum_fosc(:,:,1) = frame_fosc(1:2:end, 1:2:end, 1);
 for i=2:N
-    frame = rgb2gray(readFrame(video_llum));
-    acum(:,:,i) = frame(1:2:end, 1:2:end, 1);
+    frame_llum = rgb2gray(readFrame(video_llum));
+    acum_llum(:,:,i) = frame_llum(1:2:end, 1:2:end, 1);
+    frame_fosc = rgb2gray(readFrame(video_fosc));
+    acum_fosc(:,:,i) = frame_fosc(1:2:end, 1:2:end, 1);
 end
-fsr = sum(acum(:,:,1:100),3)/(N/2); %vs
+fsr_llum = sum(acum_llum(:,:,1:100),3)/(N/2); %vs
+fsr_fosc = sum(acum_fosc(:,:,1:100),3)/(N/2);
 
-fhdr = sum(acum(:,:,101:200),3);
-fr = acum(:,:,101);
+fhdr_llum = sum(acum_llum(:,:,101:200),3);
+fr_llum = acum_llum(:,:,101);
 
-ruido = fr-fsr;
-ruidohdr = fhdr-fsr;
+fhdr_fosc = sum(acum_fosc(:,:,101:200),3);
+fr_fosc = acum_fosc(:,:,101);
+
+ruido_llum = fr_llum-fsr_llum;
+ruidohdr_llum = fhdr_llum-fsr_llum;
+
+ruido_fosc = fr_fosc-fsr_fosc;
+ruidohdr_fosc = fhdr_fosc-fsr_fosc;
 
 
 figure, hold on
-plot(acum(120,:,1)), plot(acum(120,:,50)), plot(acum(120,:,100)), plot(fsr(120,:,1), "LineWidth", 2.0)
+plot(acum_llum(120,:,1)), plot(acum_llum(120,:,50)), plot(acum_llum(120,:,100)), plot(fsr_llum(120,:,1), "LineWidth", 2.0)
 legend({"frame_{1}","frame_{50}", "frame_{100}", "mean"})
 
 figure, hold on
-plot(acum(120,:,1)), plot(acum(120,:,50)), plot(acum(120,:,100)), plot(fsr(120,:,1), "LineWidth", 2.0)
+plot(acum_llum(120,:,1)), plot(acum_llum(120,:,50)), plot(acum_llum(120,:,100)), plot(fsr_llum(120,:,1), "LineWidth", 2.0)
 legend({"frame_{1}","frame_{50}", "frame_{100}", "mean"})
 axis([250, 730, 190, 230])
 
-algo = max(fsr)/std(fr-fsr);
+figure, hold on
+plot(acum_fosc(120,:,1)), plot(acum_fosc(120,:,50)), plot(acum_fosc(120,:,100)), plot(fsr_fosc(120,:,1), "LineWidth", 2.0)
+legend({"frame_{1}","frame_{50}", "frame_{100}", "mean"})
+
+figure, hold on
+plot(acum_fosc(120,:,1)), plot(acum_fosc(120,:,50)), plot(acum_fosc(120,:,100)), plot(fsr_fosc(120,:,1), "LineWidth", 2.0)
+legend({"frame_{1}","frame_{50}", "frame_{100}", "mean"})
+axis([250, 750, 0, 55])
+
+%soroll_llum = max(fsr_llum)/std(ruido_llum);
+%soroll_fosc = max(fsr_fosc)/std(ruido_fosc);
+soroll_llum = 255/(2*std2(ruido_llum))
+soroll_fosc = 255/(2*std2(ruido_fosc))
+soroll_llum_rang = 25500 / (2*std2(fr_llum-fhdr_llum))
+soroll_fosc_rang = 25500 / (2*std2(fr_fosc-fhdr_fosc))
